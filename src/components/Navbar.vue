11f5ff3d0b15b88f78e05039f5309e856c3ed75b
diegocmd20 Home <diegocmd20@gmail.com>
2025-04-12 19:41:40 -0400

    Navbar component added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav>
+        <a href="#"><img src="" alt=""></a>
+        <a href="">Todos los Productos</a>
+        <a href="">Contacto</a>
+    </nav>
+</template>
+
+<script>
+
+
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+
+        };
+    }
+};
+</script>
+
+<style scoped>
+nav {
+    display: flex;
+}
+
+nav > img {
+    object-fit: contain;
+}
+
+nav > a {
+    padding: 15 px;
+}
+</style>
